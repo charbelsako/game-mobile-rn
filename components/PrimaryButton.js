b3bc7cf49cc78106dc346a7439a8caeb1e1448f3
Charbel Sarkis <charbelsako>
2024-09-10 21:11:05 +0300

    Another commit

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,9 +1,42 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 export default function PrimaryButtons(props) {
+  const pressHandler = () => {
+    // console.log('press');
+  };
+
   return (
-    <View>
-      <Text>{props.children}</Text>
+    <View style={styles.outerContainer}>
+      <Pressable
+        onPress={pressHandler}
+        android_ripple={{ color: '#640233' }}
+        style={({ pressed }) =>
+          pressed ? [styles.container, styles.pressed] : styles.container
+        }
+      >
+        <Text style={styles.text}>{props.children}</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  container: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  text: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,9 +4,21 @@ import PrimaryButton from '../components/PrimaryButton';
 export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput placeholder='Enter some text' />
-      <PrimaryButton>Text</PrimaryButton>
-      <PrimaryButton>Reset this bitch</PrimaryButton>
+      <TextInput
+        style={styles.input}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCorrect={false}
+        autoCapitalize={false}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Start</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
@@ -18,7 +30,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: 100,
     elevation: 8,
-    backgroundColor: '#72063c',
+    backgroundColor: '#3b021f',
     shadowColor: 'black',
     shadowOffset: {
       width: 0,
@@ -26,5 +38,23 @@ const styles = StyleSheet.create({
     },
     shadowRadius: 10,
     shadowOpacity: 0.25,
+    alignItems: 'center',
+  },
+  input: {
+    height: 50,
+    width: 50,
+    textAlign: 'center',
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
